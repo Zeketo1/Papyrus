25bd960baab14316dce6df282ec4d6c1f257908d
zeketo <ikorofrancis24@gmail.com>
2024-11-12 19:38:50 +0100

    link

--- src/components/home/Stars.jsx
+++ src/components/home/Stars.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ShootingStars from "../../utils/ShootingStars";
 import { StarsBackground } from "../../utils/ShootingStarBg";
 export const ShootingStarsAndStarsBackgroundDemo = ({ className }) => {
-    return (
-        <div
-            className={`${className} h-full bg-black flex flex-col items-center justify-center w-full absolute`}
-        >
-            <ShootingStars />
-            <StarsBackground />
-        </div>
-    );
+  return (
+    <div
+      className={`${className} h-full bg-black flex flex-col items-center justify-center w-full absolute`}
+    >
+      <ShootingStars />
+      <StarsBackground />
+    </div>
+  );
 };
